b9899a8c6bff561baf326db6a5b73661f8213723
rascada <carftjr@gmail.com>
2016-03-13 13:50:13 +0100

    disableable subscription

--- src/components/email-subscription.vue
+++ src/components/email-subscription.vue
@@ -1,10 +1,13 @@
 <template lang='jade'>
 .subscription
-  form(v-el:form v-on:submit.prevent="subscription").email
-    h1 subskrypcja pocztowa
+  button(v-show='!active' @click.prevent='active = true') +
+  form(v-el:form v-show='active' v-on:submit.prevent="subscription").email
+    h1 Subskrypcja pocztowa
     h2(v-show='status') Udało się
     input(name='email' type='email' @keyup='status = null')
-    button Subskrybuj
+    .buttons
+      paper-button(raised @click.prevent='active = false') nie dziękuje
+      paper-button(raised) Subskrybuj
 </template>
 
 <script>
@@ -26,6 +29,7 @@ export default {
 
   data() {
     return {
+      active: true,
       status: null,
     };
   },
@@ -53,17 +57,45 @@ export default {
     left @bottom
     position absolute
 
-    padding 1.5em
     @extend .section
     border-radius .5em
-    border .15em solid rgb(91, 193, 59)
+
+    h1
+      margin .5em .25em
+      margin-top 0
 
     input
       box-shadow 0 .2em .25em rgba(#000, .5)
       border-radius .2em
-      padding .5em .75em
+      padding .75em
+      margin 0 1.75em
       font-weight 600
       outline none
       color #333
       border 0
+
+    .buttons
+      flex styl
+      margin .5em 0
+      justify around
+
+      paper-button
+        font-weight 600
+        background #fff
+        margin .5em 0
+
+  & > button
+    position absolute
+    bottom 1em
+    left 1em
+
+    cursor pointer
+    border 0
+    background #fff
+    font-size 1.5em
+    padding .5em
+    width 2em
+    border-radius 50%
+    color #222
+    box-shadow 0 .1em .2em rgba(#333, .3)
 </style>
