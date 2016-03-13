116312dc50288fe117fdfe8de03bd2e6bc3e757a
rascada <carftjr@gmail.com>
2016-03-13 17:48:11 +0100

    icon packs support

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -44,6 +44,18 @@ module.exports = {
         loader: 'json',
       },
       {
+        test: /\.css$/,
+        loader: "style!css",
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader",
+      },
+      {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'url',
         query: {
