6a4863a7b67c975efa1e364d26434caee39f85bc
damian <carftjr@gmail.com>
2017-01-10 00:37:06 +0100

    basic-weather route

---
+++ src/app/pogoda/basic-weather/basic-weather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-weather',
+  templateUrl: './basic-weather.component.html',
+  styleUrls: ['./basic-weather.component.scss']
+})
+export class BasicWeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
