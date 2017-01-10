9a2379dd53f1a860535c463b2fbd0872577205d0
damian <carftjr@gmail.com>
2017-01-10 02:24:04 +0100

    thermometer come to live

--- src/app/pogoda/thermometer/thermometer-sensor-value/thermometer-sensor-value.component.ts
+++ src/app/pogoda/thermometer/thermometer-sensor-value/thermometer-sensor-value.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-thermometer-sensor-value',
   templateUrl: './thermometer-sensor-value.component.html',
   styleUrls: ['./thermometer-sensor-value.component.scss']
 })
-export class ThermometerSensorValueComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ThermometerSensorValueComponent{
+  @Input() temperature;
+  @Input() unit;
 }
--- src/app/pogoda/thermometer/thermometer-sensor/thermometer-sensor.component.ts
+++ src/app/pogoda/thermometer/thermometer-sensor/thermometer-sensor.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-thermometer-sensor',
   templateUrl: './thermometer-sensor.component.html',
   styleUrls: ['./thermometer-sensor.component.scss']
 })
-export class ThermometerSensorComponent { }
+export class ThermometerSensorComponent {
+  @Input() temperature;
+
+  get height() {
+    return `${Math.abs(this.temperature * 1.9) + 18}%`;
+  }
+}
