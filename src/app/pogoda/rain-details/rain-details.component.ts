04035a565f5ec8445b922f6a36f2d24ed44ee4ed
damian <carftjr@gmail.com>
2017-01-11 20:12:17 +0100

    rain raport

---
+++ src/app/pogoda/rain-details/rain-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rain-details',
+  templateUrl: './rain-details.component.html',
+  styleUrls: ['./rain-details.component.css']
+})
+export class RainDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
