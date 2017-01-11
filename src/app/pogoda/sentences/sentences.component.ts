fe36bca43bc746552e2c12ba8438241d8d620829
damian <carftjr@gmail.com>
2017-01-11 21:06:38 +0100

    primitive sentences animation

--- src/app/pogoda/sentences/sentences.component.ts
+++ src/app/pogoda/sentences/sentences.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, transition, animate, style, state } from '@angular/core';
 import { SentencesService } from "./sentences.service";
 
 @Component({
   selector: 'app-sentences',
   templateUrl: './sentences.component.html',
-  styleUrls: ['./sentences.component.css']
+  styleUrls: ['./sentences.component.css'],
+  animations: [
+    trigger('state', [
+      transition('show <=> hide', animate('1000ms')),
+      state('show', style({ opacity: 1 })),
+      state('hide', style({ opacity: 0 })),
+    ]),
+  ],
 })
 export class SentencesComponent implements OnInit {
   constructor(private api: SentencesService) { }
   private sentences: string[] = [];
   private current: number = 0;
+  public state = 'show';
 
   get sentence() {
     const { sentences, current } = this;
@@ -27,11 +35,18 @@ export class SentencesComponent implements OnInit {
     });
   }
 
-  private next() {
-    const { sentences, current } = this;
-    const next = current + 1;
+  showNew({ toState }) {
+    if (toState === 'hide') {
+      const { sentences, current } = this;
+      const next = current + 1;
 
-    this.current = sentences.length > next ? next : 0;
-    setTimeout(() => this.next(), this.readTime);
+      this.current = sentences.length > next ? next : 0;
+      setTimeout(() => this.next(), this.readTime);
+      this.state = 'show';
+    }
+  }
+
+  private next() {
+    this.state = 'hide';
   }
 }
