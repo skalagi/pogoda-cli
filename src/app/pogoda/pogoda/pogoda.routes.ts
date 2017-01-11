34fb0bb5b102a8e134a0b8c902281cf2c559a086
damian <carftjr@gmail.com>
2017-01-11 20:13:34 +0100

    add raport routes

--- src/app/pogoda/pogoda/pogoda.routes.ts
+++ src/app/pogoda/pogoda/pogoda.routes.ts
@@ -1,9 +1,15 @@
-import { Routes } from "@angular/router";
-
-import { BasicWeatherComponent } from "../basic-weather/basic-weather.component";
 import { TemperatureDetailsComponent } from "../temperature-details/temperature-details.component";
+import { PressureDetailsComponent } from "../pressure-details/pressure-details.component";
+import { HumidityDetailsComponent } from "../humidity-details/humidity-details.component";
+import { RainDetailsComponent } from "../rain-details/rain-details.component";
+import { WindDetailsComponent } from "../wind-details/wind-details.component";
+import { BasicWeatherComponent } from "../basic-weather/basic-weather.component";
 
 export const pogodaRoutes = [
   { path: '', exact: true, component: BasicWeatherComponent },
   { path: 'temperatura', component: TemperatureDetailsComponent },
+  { path: 'wilgotnosc', component: HumidityDetailsComponent },
+  { path: 'cisnienie', component: PressureDetailsComponent },
+  { path: 'opady', component: RainDetailsComponent },
+  { path: 'wiatr', component: WindDetailsComponent },
 ];
