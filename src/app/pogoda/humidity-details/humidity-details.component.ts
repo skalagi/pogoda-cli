0f119a3ef4a8bc48b3be8237a5107062ed485c08
damian <carftjr@gmail.com>
2017-01-11 20:12:59 +0100

    humidity raport

---
+++ src/app/pogoda/humidity-details/humidity-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-humidity-details',
+  templateUrl: './humidity-details.component.html',
+  styleUrls: ['./humidity-details.component.css']
+})
+export class HumidityDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
