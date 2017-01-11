68f85b96af4deff705c6a148e2c0f12c938c3cee
damian <carftjr@gmail.com>
2017-01-11 23:35:53 +0100

    update day-records module

--- src/app/pogoda/day-records/day-records.module.ts
+++ src/app/pogoda/day-records/day-records.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DayRecordsService } from "./day-records.service";
 import { DayRecordsComponent } from './day-records/day-records.component';
+import { MaterialModule } from "@angular/material";
+import { DayRecordComponent } from './day-record/day-record.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule.forRoot()],
   providers: [DayRecordsService],
-  declarations: [DayRecordsComponent],
+  declarations: [DayRecordsComponent, DayRecordComponent],
   exports: [DayRecordsComponent],
 })
 export class DayRecordsModule { }
