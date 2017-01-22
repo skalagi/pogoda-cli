dc04afdc0126cbe39c98d2317aaebf88eaf88a9e
damian <carftjr@gmail.com>
2017-01-22 23:28:11 +0100

    wind day records

--- src/app/pogoda/basic-weather/wind/wind.module.ts
+++ src/app/pogoda/basic-weather/wind/wind.module.ts
@@ -7,9 +7,17 @@ import { PortableValueModule } from "../../portable-value/portable-value.module"
 import { MaterialModule } from "@angular/material";
 import { ChartsModule } from "../../charts/charts.module";
 import { RouterModule } from "@angular/router";
+import { RecordsModule } from "../../records/records.module";
 
 @NgModule({
-  imports: [RouterModule, CommonModule, PortableValueModule, MaterialModule.forRoot(), ChartsModule],
+  imports: [
+    MaterialModule.forRoot(),
+    PortableValueModule,
+    RecordsModule,
+    ChartsModule,
+    RouterModule,
+    CommonModule,
+  ],
   declarations: [WindComponent, WindDetailsComponent, WindGroupComponent],
   exports: [WindComponent],
 })
