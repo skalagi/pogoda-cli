c5b16777c71bbf6acae1d641bf04fb9a8fd80b32
damian <carftjr@gmail.com>
2017-01-24 00:03:28 +0100

    basic weather info in info-bar

--- src/app/pogoda/info-bar/info-bar.component.ts
+++ src/app/pogoda/info-bar/info-bar.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { BasicService } from "../api/basic/basic.service";
 import { BasicApi } from "../api/basic/basic.interface";
+import { Unit, WindUnit } from "../api/unit.interface";
 
 @Component({
   selector: 'info-bar',
   templateUrl: './info-bar.component.html',
-  styleUrls: ['./info-bar.component.css']
+  styleUrls: ['./info-bar.component.scss']
 })
 export class InfoBarComponent implements OnInit {
   public recordsAmount: number = 0;
+  public temperature: Unit;
+  public pressure: Unit;
+  public wind: WindUnit;
 
   constructor(private basic: BasicService) {}
 
   ngOnInit() {
     this.basic.get((api: BasicApi) => {
       this.recordsAmount = api.count;
+
+      this.temperature = api.temperature.current;
+      this.pressure = api.barometer.current;
+      this.wind = api.wind.current;
     });
   }
 }
