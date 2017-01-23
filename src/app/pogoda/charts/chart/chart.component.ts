c017cb3babad2adefc32fd4706926a77f1d10132
damian <carftjr@gmail.com>
2017-01-23 10:56:38 +0100

    reactive chart title
    close #14

--- src/app/pogoda/charts/chart/chart.component.ts
+++ src/app/pogoda/charts/chart/chart.component.ts
@@ -21,9 +21,18 @@ export class ChartComponent implements OnInit {
     series: [],
   };
 
+  get polRange() {
+    switch (this.range) {
+      case 'day': return 'dzienny';
+      case 'month': return 'miesięczny';
+      case 'year': return 'roczny';
+    }
+  }
+
   ngOnInit() {
     this.api.get(this.range, this.type).subscribe(data => {
       this.options.chart.type = data[0].length > 2 ? 'arearange' : 'line';
+      this.options.title.text = `Raport ${ this.polRange }`;
       this.options.series = [{ name: this.dataType, data }];
       this.loaded = true;
     });
