174d9dc067271830e9506ab1d83435fb08b96f8b
damian <carftjr@gmail.com>
2017-01-23 18:54:17 +0100

    moment, now records

--- src/app/pogoda/records/records.module.ts
+++ src/app/pogoda/records/records.module.ts
@@ -4,9 +4,10 @@ import { PortableValueModule } from "../portable-value/portable-value.module";
 import { MaterialModule } from "@angular/material";
 import { RecordComponent } from "./record/record.component";
 import { RecordsComponent } from './records/records.component';
+import { MomentModule } from "angular2-moment";
 
 @NgModule({
-  imports: [CommonModule, MaterialModule.forRoot(), PortableValueModule],
+  imports: [CommonModule, MaterialModule.forRoot(), PortableValueModule, MomentModule],
   declarations: [RecordComponent, RecordsComponent],
   exports: [RecordComponent, RecordsComponent],
 })
