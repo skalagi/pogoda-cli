2377e40fb3dc8d424178829d63a50a048c0c1548
rascada <carftjr@gmail.com>
2018-05-09 03:41:26 +0200

    migrate update-timer component

--- src/app/pogoda/pogoda.module.ts
+++ src/app/pogoda/pogoda.module.ts
@@ -1,29 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from "@angular/material";
 import { MomentModule } from 'angular2-moment';
 
 import { PogodaComponent } from './pogoda/pogoda.component';
 import { HeaderComponent } from './header/header.component';
 import { UpdateTimerComponent } from './update-timer/update-timer.component';
 import { SentencesComponent } from './sentences/sentences.component';
-import { PortableValueModule } from "./portable-value/portable-value.module";
-import { BasicWeatherModule } from "./basic-weather/basic-weather.module";
-import { RouterModule } from "@angular/router";
-import { pogodaRoutes } from "./pogoda/pogoda.routes";
-import { ApiModule } from "./api/api.module";
+import { PortableValueModule } from './portable-value/portable-value.module';
+import { BasicWeatherModule } from './basic-weather/basic-weather.module';
+import { RouterModule } from '@angular/router';
+import { pogodaRoutes } from './pogoda/pogoda.routes';
+import { ApiModule } from './api/api.module';
 import { InfoBarComponent } from './info-bar/info-bar.component';
 
 @NgModule({
   exports: [PogodaComponent],
   imports: [
     RouterModule.forRoot(pogodaRoutes),
-    MaterialModule.forRoot(),
     PortableValueModule,
     BasicWeatherModule,
     CommonModule,
-    MomentModule,
-    ApiModule,
   ],
 
   declarations: [
---
+++ src/app/pogoda/update-timer/update-timer.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UpdateTimerComponent } from './update-timer.component';
+import { MatProgressSpinnerModule, MatTooltipModule } from '@angular/material';
+import { MomentModule } from 'angular2-moment';
+import { ApiModule } from '../api/api.module';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        MatProgressSpinnerModule,
+        MomentModule,
+        ApiModule,
+        MatTooltipModule,
+    ],
+
+    exports: [UpdateTimerComponent],
+    declarations: [UpdateTimerComponent],
+})
+export class PogodaModule { }
