2fcc54e93c90f51e78b808974fd18480dc7f5ccf
rascada <carftjr@gmail.com>
2018-05-09 03:41:07 +0200

    little fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
-import { PogodaModule } from "./pogoda/pogoda.module";
+import { PogodaModule } from './pogoda/pogoda.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
