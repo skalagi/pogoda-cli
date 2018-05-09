9cc51bea727486bd9603d394a6a70c96df2823ad
rascada <carftjr@gmail.com>
2018-05-09 04:08:13 +0200

    migrate charts

--- src/app/pogoda/charts/charts.module.ts
+++ src/app/pogoda/charts/charts.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChartModule } from "angular2-highcharts";
-import { ChartComponent } from "./chart/chart.component";
-import { MaterialModule } from "@angular/material";
+import { ChartModule } from 'angular2-highcharts';
+import { ChartComponent } from './chart/chart.component';
 import { ReportComponent } from './report/report.component';
+import { MatCardModule, MatTabsModule, MatProgressSpinnerModule } from '@angular/material';
+import { ApiModule } from '../api/api.module';
 
 const Highcharts = require('../../../../node_modules/highcharts/highcharts');
 const HighchartsMore = require('../../../../node_modules/highcharts/highcharts-more');
@@ -11,7 +12,7 @@ const HighchartsMore = require('../../../../node_modules/highcharts/highcharts-m
 HighchartsMore(Highcharts);
 
 @NgModule({
-  imports: [CommonModule, ChartModule, MaterialModule],
+  imports: [CommonModule, ChartModule, MatCardModule, MatTabsModule, ApiModule, MatProgressSpinnerModule],
   declarations: [ChartComponent, ReportComponent],
   exports: [ChartComponent, ReportComponent],
 })
