0df9d713a2ca08b9c5cc9ebb8b40074ed5bc4894
Damian <martyniak.innovative@gmail.com>
2018-10-11 21:58:08 +0200

    reuse chart for ranges

--- src/app/pogoda/charts/report/report.component.ts
+++ src/app/pogoda/charts/report/report.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -10,8 +11,13 @@ import { ActivatedRoute } from '@angular/router';
 export class ReportComponent implements OnInit {
   @Input() public dataType = '';
   @Input() public type = '';
+  range$ = new BehaviorSubject('day');
 
   constructor(private route: ActivatedRoute) { }
+
+  changeRange(index) {
+    this.range$.next(['day', 'month', 'year'][index]);
+  }
 
   private getType(type) {
     switch (type) {
