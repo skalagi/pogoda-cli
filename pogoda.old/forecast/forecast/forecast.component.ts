9ea72824ae2523e9330a6744fd2f587d9875f6e6
Damian <martyniak.innovative@gmail.com>
2018-10-11 21:00:10 +0200

    remove old unnecesary files

--- pogoda.old/forecast/forecast/forecast.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'forecast',
-  templateUrl: './forecast.component.html',
-  styleUrls: ['./forecast.component.css']
-})
-export class ForecastComponent {}
