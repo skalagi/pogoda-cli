39f78db194240db54130e7f2b1aacb1c325a0eb4
Damian <martyniak.innovative@gmail.com>
2018-10-11 12:42:49 +0200

    refactor records

--- src/app/pogoda/records/record/record.component.ts
+++ src/app/pogoda/records/record/record.component.ts
@@ -1,20 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { RecordsQuery } from '../state';
+import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'skalagi-record',
   templateUrl: './record.component.html',
-  styleUrls: ['./record.component.css']
+  styleUrls: ['./record.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RecordComponent implements OnInit {
-  @Input('record') record: string;
-  @Input('range') range: string;
-  data$;
-
-  constructor(private query: RecordsQuery) {}
-
-  ngOnInit() {
-    const { range, record } = this;
-    this.data$ = this.query.record$(range, record);
-  }
+export class RecordComponent {
+  @Input() data$;
 }
--- src/app/pogoda/records/records/records.component.ts
+++ src/app/pogoda/records/records/records.component.ts
@@ -1,13 +1,23 @@
-import { Component, Input } from '@angular/core';
-import { RecordsService } from '../state';
+import { Component, Input, OnInit } from '@angular/core';
+import { RecordsService, RecordsQuery } from '../state';
 
 @Component({
   selector: 'skalagi-records',
   templateUrl: './records.component.html',
   styleUrls: ['./records.component.css']
 })
-export class RecordsComponent {
+export class RecordsComponent implements OnInit {
+  ranges: String[] = ['day', 'month', 'year'];
   @Input() public record: string;
+  month$;
+  year$;
+  day$;
 
-  constructor(private _records: RecordsService) { }
+  constructor(private _records: RecordsService, private query: RecordsQuery) { }
+
+  ngOnInit() {
+    this.ranges.forEach(range => {
+      this[`${range}$`] = this.query.record$(range, this.record);
+    });
+  }
 }
