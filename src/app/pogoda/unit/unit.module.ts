cb78dc232ce8e755b5c89fe0912c9ff443e40a66
Damian <martyniak.innovative@gmail.com>
2018-10-11 14:43:37 +0200

    nicer unit

--- src/app/pogoda/unit/unit.module.ts
+++ src/app/pogoda/unit/unit.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UnitComponent } from './unit/unit.component';
-import { GridModule } from '@angular/flex-layout';
+import { GridModule, FlexModule } from '@angular/flex-layout';
 import { PortableValueModule } from '../portable-value/portable-value.module';
 import { MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    GridModule,
+    FlexModule,
     MatIconModule,
     PortableValueModule,
   ],
