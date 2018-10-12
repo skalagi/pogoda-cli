959e5ec7fbcef9dbd8bafdcae639a31c57f4d92c
Damian <martyniak.innovative@gmail.com>
2018-10-12 15:12:34 +0200

    setup highcharts i18y and modules for whole app

--- src/app/pogoda/charts/charts.module.ts
+++ src/app/pogoda/charts/charts.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChartModule, Highcharts } from 'angular-highcharts';
+import { ChartModule } from 'angular-highcharts';
 import { ChartComponent } from './chart/chart.component';
 import { ReportComponent } from './report/report.component';
 import {
@@ -9,19 +9,6 @@ import {
   MatProgressBarModule
 } from '@angular/material';
 import { RouterModule } from '@angular/router';
-
-require('highcharts/highcharts-more')(Highcharts);
-require('highcharts/modules/accessibility')(Highcharts);
-
-Highcharts.setOptions({
-  lang: {
-    months: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień',
-    'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień',
-    'Październik', 'Listopad', 'Grudzień'],
-    shortMonths: ['sty', 'lut', 'mar', 'kwi', 'maj', 'cze', 'lip', 'sie', 'wrz', 'paź', 'lis', 'gru'],
-    weekdays: ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'],
-  }
-});
 
 @NgModule({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import './polyfills.ts';
 import 'moment/min/locales';
-import * as moment from 'moment';
 
+import * as moment from 'moment';
 moment.locale('pl-pl');
+
+import { Highcharts } from 'angular-highcharts';
+require('highcharts/highcharts-more')(Highcharts);
+require('highcharts/modules/accessibility')(Highcharts);
+
+Highcharts.setOptions({
+  lang: {
+    months: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień',
+    'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień',
+    'Październik', 'Listopad', 'Grudzień'],
+    shortMonths: ['sty', 'lut', 'mar', 'kwi', 'maj', 'cze', 'lip', 'sie', 'wrz', 'paź', 'lis', 'gru'],
+    weekdays: ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'],
+  }
+});
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
