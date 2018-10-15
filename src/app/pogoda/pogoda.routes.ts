73ff10f4b6f2f2d4f66638fff7d772464fb8a4c6
Damian <martyniak.innovative@gmail.com>
2018-10-15 21:13:46 +0200

    change routing a little

--- src/app/pogoda/pogoda.routes.ts
+++ src/app/pogoda/pogoda.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const pogodaRoutes: Routes = [
-  { path: '', pathMatch: 'full', loadChildren: './basic/basic-weather.module#BasicWeatherModule' },
+  { path: '', pathMatch: 'full', redirectTo: 'pomiary' },
+  { path: 'pomiary', loadChildren: './basic/basic-weather.module#BasicWeatherModule' },
   { path: 'raport', loadChildren: './charts/charts.module#ChartsModule' },
   /*
   { path: 'temperatura', component: TemperatureDetailsComponent },
