d8d12cafb738a414422deadf9bf5272a32439517
Damian <martyniak.innovative@gmail.com>
2018-10-15 22:06:42 +0200

    add routes to header and footer

--- src/app/pogoda/pogoda/footer/footer.module.ts
+++ src/app/pogoda/pogoda/footer/footer.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatListModule } from '@angular/material';
 import { FlexModule } from '@angular/flex-layout/flex';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatListModule,
+    RouterModule,
     FlexModule,
     MatButtonModule,
   ],
