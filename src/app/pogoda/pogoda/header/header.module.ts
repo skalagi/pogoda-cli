95a19e093f73c60bea2f44ffe592ee9e0b666e71
Damian <martyniak.innovative@gmail.com>
2018-10-15 14:58:10 +0200

    restyle header

--- src/app/pogoda/pogoda/header/header.module.ts
+++ src/app/pogoda/pogoda/header/header.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
+import { FlexModule } from '@angular/flex-layout/flex';
 
 @NgModule({
     imports: [
         CommonModule,
         UpdateTimerModule,
         RouterModule,
+        FlexModule,
         MatButtonModule,
         MatToolbarModule,
         MatIconModule,
