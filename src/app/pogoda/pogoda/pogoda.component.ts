883059ecbc4dd82313ea590db92fe796e348c322
Damian <martyniak.innovative@gmail.com>
2018-10-16 23:27:44 +0200

    add ga

--- src/app/pogoda/pogoda/pogoda.component.ts
+++ src/app/pogoda/pogoda/pogoda.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SEOService } from '../seo.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'skalagi-pogoda',
@@ -7,9 +9,31 @@ import { SEOService } from '../seo.service';
   styleUrls: ['./pogoda.component.scss']
 })
 export class PogodaComponent implements OnInit {
-  constructor(private seo: SEOService) {}
+  constructor(private seo: SEOService, private router: Router) {}
 
   ngOnInit() {
     this.seo.createCanonical();
+
+    if (environment.production) {
+      (<any>window).ga('set', 'page', this.router.url);
+      (<any>window).ga('send', 'pageview');
+    }
+
+    this.router.events.subscribe(ev => {
+      if (ev instanceof NavigationEnd) {
+        if (environment.production) {
+          (<any>window).ga('set', 'page', ev.urlAfterRedirects);
+          (<any>window).ga('send', 'pageview');
+        }
+      }
+    });
+
+    /*
+      this.sw.available.subscribe(() => {
+        if (confirm('Dostępna nowa wersja strony. Wczytać?')) {
+          location.reload();
+        }
+      });
+    */
   }
 }
