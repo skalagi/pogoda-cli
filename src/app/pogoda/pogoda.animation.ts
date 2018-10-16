c84b5d211a82db3a91653304e5484a3be91cdb40
Damian <martyniak.innovative@gmail.com>
2018-10-16 23:06:02 +0200

    different pogoda animation

--- src/app/pogoda/pogoda.animation.ts
+++ src/app/pogoda/pogoda.animation.ts
@@ -1,19 +1,13 @@
 import { trigger, style, state, animate, transition } from '@angular/animations';
 
 export const slideTrigger = trigger('slide', [
-    state('closed', style({
-        height: 0,
-    })),
+    state('void', style({ height: 0 })),
 
-    state('void', style({
-        height: 0,
-    })),
+    transition(':leave', [
+      animate('.4s cubic-bezier(0.4, 0.0, 0.2, 1)', style({ height: 0 }))
+    ]),
 
-    state('open, *', style({
-        height: '*',
-    })),
-
-    transition('* <=> *', [
-        animate('.4s cubic-bezier(0.4, 0.0, 0.2, 1)'),
+    transition(':enter', [
+      animate('.4s cubic-bezier(0.4, 0.0, 0.2, 1)', style({ height: '*' })),
     ]),
 ]);
