2285f6ae2e5347969661f630edefaebf776c95c1
Damian <martyniak.innovative@gmail.com>
2018-10-16 02:36:24 +0200

    update timer

--- src/app/pogoda/update-timer/update-timer.component.ts
+++ src/app/pogoda/update-timer/update-timer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Unit } from '../unit/unit';
 import { BasicWeatherQuery } from '../basic/state';
+import { BehaviorSubject, interval } from 'rxjs';
+import { timeInterval } from 'rxjs/operators';
 
 @Component({
   selector: 'skalagi-update-timer',
@@ -8,13 +9,21 @@ import { BasicWeatherQuery } from '../basic/state';
   styleUrls: ['./update-timer.component.scss']
 })
 export class UpdateTimerComponent implements OnInit {
-  public loaded = false;
-  public data: Unit;
-  public time = 0;
+  loading$ = this.query.selectLoading();
+  time$ = new BehaviorSubject(null);
 
   constructor(private query: BasicWeatherQuery) { }
 
-  ngOnInit() {
+  tick() {
+    const { value } = this.time$;
 
+    if (value) {
+      this.time$.next(value - 1);
+    }
+  }
+
+  ngOnInit() {
+    this.query.basic('time').subscribe(time => time ? this.time$.next(time.next.value) : time);
+    interval(1000).subscribe(() => this.tick());
   }
 }
