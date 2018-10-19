8bdcb801b7dee6614fd84166877480e2d5813ea8
Damian <martyniak.innovative@gmail.com>
2018-10-19 13:39:40 +0200

    remove sticky from sentences

--- src/app/pogoda/sentences/sentences.module.ts
+++ src/app/pogoda/sentences/sentences.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SentencesComponent } from './sentences.component';
 import { MatProgressBarModule } from '@angular/material';
-import { FlexModule } from '@angular/flex-layout/flex';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
-    FlexModule,
+    FlexLayoutModule,
     MatProgressBarModule,
     MatProgressBarModule,
   ],
