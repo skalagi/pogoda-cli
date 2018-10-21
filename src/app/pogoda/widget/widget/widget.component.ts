66a3d959cf7d49c30c3c1d92c7ecdf8d7857d381
Damian <martyniak.innovative@gmail.com>
2018-10-22 00:15:23 +0200

    optimize widget

--- src/app/pogoda/widget/widget/widget.component.ts
+++ src/app/pogoda/widget/widget/widget.component.ts
@@ -8,20 +8,12 @@ import { BasicWeatherQuery } from 'app/pogoda/basic/state';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WidgetComponent implements OnInit {
-  temperature$;
-  humidity$;
-  presure$;
-  wind$;
-  rain$;
+  basic$;
 
   constructor(private query: BasicWeatherQuery) { }
 
   ngOnInit() {
-    this.temperature$ = this.query.basic('temperature');
-    this.humidity$ = this.query.basic('humidity');
-    this.presure$ = this.query.basic('barometer');
-    this.wind$ = this.query.basic('wind');
-    this.rain$ = this.query.basic('rain');
+    this.basic$ = this.query.select();
   }
 
 }
