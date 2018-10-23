fa854f08835f237713da84a347714a471529c0c0
Damian <martyniak.innovative@gmail.com>
2018-10-24 01:20:01 +0200

    widget fixes and improvments

--- src/app/pogoda/widget/widget.module.ts
+++ src/app/pogoda/widget/widget.module.ts
@@ -5,11 +5,13 @@ import { WidgetUnitComponent } from './widget-unit/widget-unit.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { WidgetUnitValueComponent } from './widget-unit-value/widget-unit-value.component';
 import { MatIconModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     FlexLayoutModule,
+    RouterModule,
     MatIconModule,
   ],
   declarations: [WidgetComponent, WidgetUnitComponent, WidgetUnitValueComponent],
