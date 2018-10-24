c8c4637694379c0a5fa736d6a7831038ed189f98
Damian <martyniak.innovative@gmail.com>
2018-10-24 03:48:39 +0200

    add forecast to homepage widget

--- src/app/pogoda/widget/widget.module.ts
+++ src/app/pogoda/widget/widget.module.ts
@@ -4,14 +4,17 @@ import { WidgetComponent } from './widget/widget.component';
 import { WidgetUnitComponent } from './widget-unit/widget-unit.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { WidgetUnitValueComponent } from './widget-unit-value/widget-unit-value.component';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule, MatButtonModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
+import { MomentModule } from 'ngx-moment';
 
 @NgModule({
   imports: [
     CommonModule,
     FlexLayoutModule,
+    MatButtonModule,
     RouterModule,
+    MomentModule,
     MatIconModule,
   ],
   declarations: [WidgetComponent, WidgetUnitComponent, WidgetUnitValueComponent],
--- src/app/pogoda/widget/widget/widget.component.ts
+++ src/app/pogoda/widget/widget/widget.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 import { BasicWeatherQuery } from 'app/pogoda/basic/state';
+import { ForecastQuery } from 'app/pogoda/forecast/state';
 
 @Component({
   selector: 'skalagi-widget',
@@ -8,12 +9,14 @@ import { BasicWeatherQuery } from 'app/pogoda/basic/state';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WidgetComponent implements OnInit {
+  forecast$;
   basic$;
 
-  constructor(private query: BasicWeatherQuery) { }
+  constructor(private query: BasicWeatherQuery, private forecast: ForecastQuery) { }
 
   ngOnInit() {
     this.basic$ = this.query.select();
+    this.forecast$ = this.forecast.select();
   }
 
 }
